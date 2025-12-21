5b4e97385b2a331418315d6b62557bf7c6b2099b
Tom Ritchford <tom@swirly.com>
2025-12-21 20:23:21 +0100

    Add tuney/note.py

---
+++ tuney/note.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import dataclasses as dc
+
+
+@dc.dataclass
+class Note:
+    name: str
+    accidental: str = ""
+    octave: int | None = None
+
+    def __repr__(self) -> str:
+        octave = "" if self.octave is None else self.octave
+        return f"{self.note}{self.accidental}{octave}"
+
+    @staticmethod
+    def make(note_name: str) -> Note:
+        if 1 <= len(n := note_name) <= 3 and "A" <= (name := n[0]) <= "G":
+            n = n[1:]
+            accidental = ""
+            octave = None
+
+            if n and n[-1].isnumeric():
+                octave = int(n[-1])
+                n = n[:-1]
+            if n and n[0] in "#b":
+                accidental = n[0]
+                n = n[1:]
+            if not n:
+                return Note(name, accidental, octave)
+
+        raise ValueError(f"Cannot understand note {note_name}")
