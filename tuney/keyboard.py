47818a69155a4318cced5032575cbe96a8e30e62
Tom Ritchford <tom@swirly.com>
2026-01-02 21:14:21 +0100

    Remove old keyboard cruft

--- tuney/keyboard.py
+++ tuney/keyboard.py
@@ -52,40 +52,5 @@ ERROR = (
     " until it is added to accessibility clients."
 )
 
-
-def on_press(key):
-    print(type(key), getattr(key, "char", None))
-    print(key)
-    print(dir(key))
-    print(*vars(key))
-
-    try:
-        print("alphanumeric key {0} pressed".format(key.char))
-    except AttributeError:
-        print("special key {0} pressed".format(key))
-
-
-def on_release(key):
-    print(type(key))
-    print("{0} released".format(key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-
-# Collect events until released
-print("before")
-
-if True:
-    k = Keyboard().receive()
-
-else:
-    li = keyboard.Listener(on_press=on_press, on_release=on_release)
-    r = redirect_stderr(io.StringIO())
-    r.__enter__()
-
-    with li as listener:
-        r.__exit__(None, None, None)
-        print("middle")
-        listener.join()
-        print("after")
+if __name__ == '__main__':
+    Keyboard().receive()
