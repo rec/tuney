95848ff28715652e8b1d268e83e6574536a4d3e7
Tom Ritchford <tom@swirly.com>
2026-01-03 17:06:54 +0100

    Limit velocity and note_number in midi.py

--- tuney/midi.py
+++ tuney/midi.py
@@ -3,16 +3,19 @@ from .note import NoteOctave
 
 import mido
 
+ZERO_IS_NOTE_OFF = True
+
 
 @dc.dataclass(frozen=True)
 class NoteMaker:
     channel: int = 0
     velocity: int = 0x40
-    zero_is_note_off: bool = True
+    note_offset: int = 0
 
-    def __call__(self, note: NoteOctave, is_press: bool) -> mido.Message:
+    def message(self, note: NoteOctave, is_press: bool) -> mido.Message:
         return mido.Message(
             channel=self.channel,
-            type="note_on" if is_press or self.zero_is_note_off else "note_off",
-            velocity=is_press * self.velocity,
+            note=(note.note_number + self.note_offset) % 128,
+            type="note_on" if is_press or ZERO_IS_NOTE_OFF else "note_off",
+            velocity=max(0, min(127, is_press * self.velocity)),
         )
