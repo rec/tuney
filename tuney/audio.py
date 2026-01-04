b49a3982cfb283cacc22d89c6b3c571d4bdca1cf
Tom Ritchford <tom@swirly.com>
2026-01-04 19:53:01 +0100

    Simplify audio.py

--- tuney/audio.py
+++ tuney/audio.py
@@ -2,42 +2,23 @@ from __future__ import annotations
 
 import dataclasses as dc
 import threading
+from abc import abstractmethod
 from functools import cached_property
 from typing import TypeAlias
+from queue import Queue
 
-import sounddevice as sd
+from sounddevice import CallbackStop, OutputStream
 import soundfile as sf
 import numpy as np
-from abc import abstractmethod
 
 Data: TypeAlias = np.ndarray
 
 
-@dc.dataclass(frozen=True)
-class SampleData:
-    data: Data
-    sample_rate: int
-
-    @cached_property
-    def channels(self) -> int:
-        return self.data.shape[1]
-
-    @staticmethod
-    def from_file(filename: str) -> SampleData:
-        return SampleData(*sf.read(filename, always_2d=True))
-
-    def stream(self, **kwargs) -> sd.OutputStream:
-        return sd.OutputStream(
-            channels=self.channels,
-            samplerate=self.sample_rate,
-            **kwargs,
-        )
-
-
 @dc.dataclass
 class PlaybackBase:
-    data: SampleData
+    channels: int = 1
     device: int | str = 0
+    sample_rate: int = 0
 
     _current_frame: int = 0
     _event: threading.Event = dc.field(default_factory=threading.Event)
@@ -58,7 +39,7 @@ class PlaybackBase:
             self._running = False
 
         if not self._running:
-            raise sd.CallbackStop()
+            raise CallbackStop()
 
     def run(self):
         self._running = True
@@ -69,21 +50,35 @@ class PlaybackBase:
             self._running = False
 
     @cached_property
-    def stream(self) -> sd.OutputStream:
-        return self.data.stream(
+    def stream(self) -> OutputStream:
+        return OutputStream(
             callback=self.callback,
+            channels=self.channels,
             device=self.device,
+            samplerate=self.sample_rate,
             finished_callback=self._event.set,
         )
 
 
+@dc.dataclass
 class DataPlayback(PlaybackBase):
+    data: Data = dc.field(default_factory=Data)
+
     def next_chunk(self, frames: int) -> Data:
         return self.data.data[self._current_frame : self._current_frame + frames]
 
     @staticmethod
     def from_file(filename: str, device: int | str = 0) -> DataPlayback:
-        return DataPlayback(SampleData.from_file(filename), device)
+        data, sample_rate = sf.read(filename, always_2d=True)
+        return DataPlayback(
+            channels=data.shape[1], data=data, device=device, sample_rate=sample_rate
+        )
+
+
+@dc.dataclass
+class SynthPlayback(PlaybackBase):
+    _queue: Queue = dc.field(default_factory=Queue)
+    # @cached_property
 
 
 if __name__ == "__main__":
