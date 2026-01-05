5e096b224f77b2779b4ebb661a2354a646c747f8
Tom Ritchford <tom@swirly.com>
2026-01-05 11:16:02 +0100

    Extract __main__ out of linear_mapper.py

---
+++ tuney/__main__.py
@@ -0,0 +1,22 @@
+from .audio.file_playback import FilePlayback
+from .keyboard import KeyboardQueue
+from .linear_mapper import LinearMapper
+
+
+def main():
+    mapper = LinearMapper(note_name="C3", case_sensitive=False, invert=False)
+
+    def callback(key_action):
+        if key_action.is_press:
+            for letter, note in mapper(key_action.char):
+                if note is not None:
+                    print(note, "", end="", flush=True)
+                    name = str(note).replace("♯", "#")
+                    FilePlayback(f"assets/piano/{name}.mp3").run()
+
+    kq = KeyboardQueue(callback)
+    kq.start()
+
+
+if __name__ == "__main__":
+    main()
--- tuney/linear_mapper.py
+++ tuney/linear_mapper.py
@@ -45,25 +45,3 @@ class LinearMapper:
         if self.length:
             index = index % self.length
         return self.note.add(index + self.offset)
-
-
-def test():
-    from .keyboard import KeyboardQueue
-    from .audio.file_playback import FilePlayback
-
-    mapper = LinearMapper(note_name="C3", case_sensitive=False, invert=False)
-
-    def callback(key_action):
-        if key_action.is_press:
-            for letter, note in mapper(key_action.char):
-                if note is not None:
-                    print(note, "", end="", flush=True)
-                    name = str(note).replace("♯", "#")
-                    FilePlayback(f"assets/piano/{name}.mp3").run()
-
-    kq = KeyboardQueue(callback)
-    kq.start()
-
-
-if __name__ == "__main__":
-    test()
