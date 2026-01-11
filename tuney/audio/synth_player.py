010972062e4ab8e5ec5aa676176ff4c686a2767d
Tom Ritchford <tom@swirly.com>
2026-01-11 22:32:52 +0100

    Implement fade in and out (fix #5)

--- tuney/audio/synth_player.py
+++ tuney/audio/synth_player.py
@@ -18,12 +18,16 @@ Number: TypeAlias = int | float
 Function: TypeAlias = Callable[..., Any]
 
 INTENSITY = 0.1
+FADE = 0  # 0x40000
 
 
+# TODO: relieve the tension between human units (frequency, time) and sample units.
 @dc.dataclass(frozen=True)
 class Sound:
     period: Number = 0x100
     intensity: Number = INTENSITY
+    fade_in_samples: Number = 0x1000
+    fade_out_samples: Number = 0x1000
 
 
 @dc.dataclass(frozen=True)
@@ -37,6 +41,17 @@ class OscillatorPlayer(Player):
     sound: Sound = dc.field(default_factory=Sound)
     oscillator: Oscillator = dc.field(default_factory=Oscillator)
 
+    _stopping: bool = False
+
+    #: Records the the frame we started to fade out.
+    _fade_frame: Number | None = None
+
+    def stop(self) -> None:
+        if self.sound.fade_out_samples > 0:
+            self._stopping = True
+        else:
+            super().stop()
+
     def _fill(self, out: Data) -> bool:
         start = self.frame_count % self.sound.period
         end = start + len(out)
@@ -49,6 +64,30 @@ class OscillatorPlayer(Player):
             # Scale up from [-1, 1] for int types only
             intensity *= np.iinfo(out.dtype).max
         wave *= intensity
+
+        def clamp(x: float) -> float:
+            return max(0.0, min(1.0, x))
+
+        def fade(wave: np.ndarray, start: float, length: float) -> None:
+            wave *= np.linspace(clamp(start), clamp(start + length), len(wave))
+
+        fade_in = self.sound.fade_in_samples
+        if self.frame_count < fade_in and not self._stopping:
+            fade(wave, self.frame_count / fade_in, len(out) / fade_in)
+
+        elif self._stopping:
+            if self._fade_frame is None:
+                # Account for the case when we fade out before we've faded in
+                offset = max(0.0, fade_in - self.frame_count)
+                self._fade_frame = self.frame_count - offset
+
+            fade_out = self.sound.fade_out_samples
+            elapsed = self.frame_count - self._fade_frame
+            if (start := 1 - elapsed / fade_out) <= 0:
+                super().stop()
+                return False
+
+            fade(wave, start, -len(out) / fade_out)
 
         wave = wave.reshape((len(wave), 1))
         out[:] = np.asarray(wave, dtype=out.dtype)
@@ -93,25 +132,27 @@ def demo():
 
     oc = OscillatorController()
 
+    DT = 0.2
+
     stack = []
     o1 = "C4", "E4", "D5", "Eb3", "G3", "C3", "E3", "D4", "Eb2", "G2"
     o2 = "C2", "E2", "D3", "Eb1", "G1", "C1", "E1", "D2", "Eb0", "G0"
 
     o1 = "C3", "E3", "D4", "Eb2", "G2"
     o2 = "C1", "E1", "D2", "Eb0", "G0"
-    for note in o1 + o2:
+    for note in (o1 + o2)[0]:
         print("on", note)
         stack.append(NoteOctave.from_name(note))
         if not oc.start(stack[-1]):
             print("oops", stack[-1])
-        time.sleep(1.0)
+        time.sleep(DT)
 
     while stack:
         note = stack.pop()
         print("off", _timestamp(), note)
         if not oc.stop(note):
             print("oops off", note)
-        time.sleep(0.5)
+        time.sleep(DT / 2)
 
 
 def demo1():
