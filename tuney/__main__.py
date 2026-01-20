0f771f090066a24a409e31d9726b20a3a017aa16
Tom Ritchford <tom@swirly.com>
2026-01-20 14:51:28 +0100

    Teach LinearMapper to forget about Note

--- tuney/__main__.py
+++ tuney/__main__.py
@@ -2,27 +2,29 @@ from .audio.file_player import FilePlayer
 from .audio.synth_player import OscillatorController
 from .keyboard import KeyboardQueue
 from .linear_mapper import LinearMapper
-from .note import Note
+from .note import make_note, Note
 
 USE_FILE = not True
 
 
 def main():
-    mapper = LinearMapper(note="C3", case_sensitive=True, invert=False)
+    mapper = LinearMapper(case_sensitive=True, invert=False)
+    note_name = make_note("C3")
     oc = OscillatorController()
 
     def callback(key_action):
-        for letter, note in mapper(key_action.char):
-            if not isinstance(note, Note):
-                continue
-            elif not USE_FILE:
-                if key_action.is_press:
-                    oc.start(note)
-                else:
-                    oc.stop(note)
-            elif key_action.is_press:
-                name = str(note).replace("♯", "#")
-                FilePlayer(f"assets/piano/{name}.mp3").run()
+        if (note_number := mapper(key_action.char)) is None:
+            return
+        note = note_name.add(note_number)
+        assert isinstance(note, Note)
+        if not USE_FILE:
+            if key_action.is_press:
+                oc.start(note)
+            else:
+                oc.stop(note)
+        elif key_action.is_press:
+            name = str(note).replace("♯", "#")
+            FilePlayer(f"assets/piano/{name}.mp3").run()
 
     kq = KeyboardQueue(callback)
     kq.start()
--- tuney/linear_mapper.py
+++ tuney/linear_mapper.py
@@ -1,19 +1,11 @@
 import dataclasses as dc
 from functools import cached_property
 from string import ascii_letters, ascii_lowercase
-from typing import Iterable, Iterator, NamedTuple
-from .note import NoteName, make_note
-
-
-class LetterNote(NamedTuple):
-    letter: str
-    note: NoteName | None
 
 
 @dc.dataclass(frozen=True)
 class LinearMapper:
     alphabet_in: str | None = None
-    note: str = "C4"
     length: int = 0
     case_sensitive: bool = False
     invert: bool = False
@@ -25,23 +17,19 @@ class LinearMapper:
             return ascii_letters if self.case_sensitive else ascii_lowercase
         return self.alphabet_in
 
-    @cached_property
-    def note_name(self) -> NoteName:
-        return make_note(self.note)
+    def __call__(self, letter: str) -> int | None:
+        if len(letter) > 1:
+            return None
 
-    def __call__(self, letters: Iterable[str]) -> Iterator[LetterNote]:
-        for letter in letters:
-            yield LetterNote(letter, self.letter_to_note(letter))
-
-    def letter_to_note(self, letter: str) -> NoteName | None:
-        assert len(letter) == 1, letter
         letter = letter if self.case_sensitive else letter.lower()
         try:
             index = self.alphabet.index(letter)
         except ValueError:
             return None
+
         if self.invert:
             index = len(self.alphabet) - index - 1
         if self.length:
             index = index % self.length
-        return self.note_name.add(index + self.offset)
+
+        return index + self.offset
