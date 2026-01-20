35e1004412e964dc61d430182d992077463778b3
Tom Ritchford <tom@swirly.com>
2026-01-20 17:10:45 +0100

    Add test_scale to test_note.py

--- test/test_note.py
+++ test/test_note.py
@@ -1,6 +1,7 @@
 import pytest
 
-from tuney.note import canonical, make_note
+from tuney.note import canonical, make_note, Note
+from tuney.scale import twelve_tet as tt
 
 NOTES = ["C", "C#", "G♯", "C-2", "F♭10"]
 
@@ -10,3 +11,11 @@ def test_note(note):
     actual = str(make_note(note))
 
     assert actual == canonical(note)
+
+
+@pytest.mark.parametrize("note", NOTES)
+def test_twelve_tet(note):
+    if isinstance((n := make_note(note)), Note):
+        actual = n.note_number
+        expected = tt.name_to_number(note)
+        assert actual == expected
