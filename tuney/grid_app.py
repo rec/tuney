d2b499790f0f78a9e8a177d8a85ca478a97522f0
Tom Ritchford <tom@swirly.com>
2026-01-21 19:56:52 +0100

    Abstact the interface to NoteGrid

--- tuney/grid_app.py
+++ tuney/grid_app.py
@@ -1,3 +1,5 @@
+import dataclasses as dc
+
 from textual.app import App, ComposeResult
 from textual.widgets import Static
 from textual.color import Color
@@ -6,6 +8,38 @@ from typing import Sequence
 RED, GREY = Color.parse("orange"), Color.parse("grey")
 
 FLAT = ("C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B")
+
+
+@dc.dataclass
+class Text:
+    labels: Sequence[str]
+    on: bool = False
+
+
+
+class NoteGrid(App):
+    theme = "textual-light"
+    grid_items: Sequence[Text]
+
+    def compose(self) -> ComposeResult:
+        for t in self.text_items:
+            yield Static("\n".join(t.labels), classes="on" if t.on else "off")
+
+
+def to_columns_rows(n: int) -> tuple[int, int]:
+    rows = int(n**0.5)
+    columns = n // rows
+    return columns + ((rows * columns) < n), rows
+
+
+def make_app(items: Sequence[str]) -> App:
+    columns, rows = to_columns_rows(len(items))
+
+    g = NoteGrid()
+    css = CSS.format(columns=columns, rows=rows)
+    g.__dict__.update(text_items=items, CSS=css)
+    return g
+
 
 CSS = """
 Screen {{
@@ -26,38 +60,16 @@ Static {{
 .off {{
     border: lightblue;
     color: lightgrey;
-}}
-"""
-
-
-class NoteGrid(App):
-    theme = "textual-light"
-    text_items: Sequence[str]
-
-    def compose(self) -> ComposeResult:
-        for t in self.text_items:
-            label = "\n".join((t, t))
-            yield Static(label, classes="on" if t == "C" else "off")
-
-
-def to_columns_rows(n: int) -> tuple[int, int]:
-    rows = int(n**0.5)
-    columns = n // rows
-    return columns + ((rows * columns) < n), rows
-
-
-def make_app(items: Sequence[str]) -> App:
-    columns, rows = to_columns_rows(len(items))
-    css = CSS.format(columns=columns, rows=rows)
-
-    g = NoteGrid()
-    g.CSS = css  # ty: ignore[invalid-attribute-access]
-    g.text_items = items
-    return g
+}}"""
 
 
 if __name__ == "__main__":
     import sys
 
-    app = make_app((FLAT * 100)[: int(sys.argv[1])])
+    def text(i: int) -> Text:
+        s = FLAT[i % len(FLAT)]
+        return Text((s, s), len(s) > 1)
+
+    count = int(sys.argv[1])
+    app = make_app([text(i) for i in range(count)])
     app.run()
