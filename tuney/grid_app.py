46a8a5d14c44ee65262d77f2301b755121ec0a9f
Tom Ritchford <tom@swirly.com>
2026-01-21 20:20:04 +0100

    Simplify NoteGrid

--- tuney/grid_app.py
+++ tuney/grid_app.py
@@ -1,9 +1,10 @@
 import dataclasses as dc
+import math
 
 from textual.app import App, ComposeResult
 from textual.widgets import Static
 from textual.color import Color
-from typing import Sequence
+from typing import Any, Sequence
 
 RED, GREY = Color.parse("orange"), Color.parse("grey")
 
@@ -16,35 +17,25 @@ class Text:
     on: bool = False
 
 
-
 class NoteGrid(App):
     theme = "textual-light"
-    grid_items: Sequence[Text]
+    texts: Sequence[Text]
+
+    def __init__(self, texts: Sequence[Text], *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.texts = texts
+        css = CSS.format(columns=int(math.ceil(len(texts) ** 0.5)))
+        self.CSS = css  # ty: ignore[invalid-attribute-access]
 
     def compose(self) -> ComposeResult:
-        for t in self.text_items:
+        for t in self.texts:
             yield Static("\n".join(t.labels), classes="on" if t.on else "off")
-
-
-def to_columns_rows(n: int) -> tuple[int, int]:
-    rows = int(n**0.5)
-    columns = n // rows
-    return columns + ((rows * columns) < n), rows
-
-
-def make_app(items: Sequence[str]) -> App:
-    columns, rows = to_columns_rows(len(items))
-
-    g = NoteGrid()
-    css = CSS.format(columns=columns, rows=rows)
-    g.__dict__.update(text_items=items, CSS=css)
-    return g
 
 
 CSS = """
 Screen {{
     layout: grid;
-    grid-size: {columns} {rows};
+    grid-size: {columns};
     background: white;
 }}
 Static {{
@@ -58,7 +49,7 @@ Static {{
     text-style: bold;
 }}
 .off {{
-    border: lightblue;
+    border: solid lightblue;
     color: lightgrey;
 }}"""
 
@@ -71,5 +62,4 @@ if __name__ == "__main__":
         return Text((s, s), len(s) > 1)
 
     count = int(sys.argv[1])
-    app = make_app([text(i) for i in range(count)])
-    app.run()
+    NoteGrid([text(i) for i in range(count)]).run()
