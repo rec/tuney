30c84da0d5f84139777ce5f7585aa4c78fa2a3da
Tom Ritchford <tom@swirly.com>
2026-01-22 13:23:04 +0100

    Modify tuney/audio/oscillator.py

--- tuney/audio/oscillator.py
+++ tuney/audio/oscillator.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
 import dataclasses as dc
-from typing import Any, Callable, TypeAlias
+from abc import ABC, abstractmethod
+from typing import Any, Callable, TypeAlias, override
 
 import numpy as np
 
-from . import Number
+from . import Data, Number
 
 Function: TypeAlias = Callable[..., Any]
 
 
-@dc.dataclass(frozen=True)
 class Oscillator:
-    function: Function = np.sin
     period: Number = 2 * np.pi
+
+    def function(self, x: Data, out: Data) -> Data:
+        return np.sin(x, out=out)
