17b78a58652e1b7c4cc4355c23fe60c965ecffd5
Tom Ritchford <tom@swirly.com>
2026-01-23 18:27:59 +0100

    Split up keyboard/

---
+++ tuney/keyboard/listener.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import dataclasses as dc
+import sys
+import threading
+import traceback
+from functools import cached_property, wraps
+from queue import Empty, Queue
+from typing import Any, Callable, TypeAlias
+
+from pynput import keyboard
+
+from . import Callback, Key, KeyAction
+
+
+@dc.dataclass
+class KeyboardListener:
+    callback: Callback
+    stop_key: keyboard.Key = keyboard.Key.esc
+
+    @cached_property
+    def listener(self) -> keyboard.Listener:
+        return _make_listener(self)
+
+    def on_press(self, key: Key | None) -> bool | None:
+        if key == self.stop_key:
+            return False
+        self._on(key, True)
+
+    def on_release(self, key: Key | None) -> None:
+        self._on(key, False)
+
+    def start(self) -> None:
+        with self.listener:
+            try:
+                self.listener.join()
+            finally:
+                self.callback(KeyAction())
+
+    def _on(self, key: Key | None, is_press: bool) -> None:
+        if char := getattr(key, "char", ""):
+            self.callback(KeyAction(char, is_press))
+
+
+def _make_listener(kl: KeyboardListener) -> keyboard.Listener:
+    listener = keyboard.Listener(
+        on_press=kl.on_press,
+        on_release=kl.on_release,
+    )
+    log = getattr(listener, "_log", None)
+    if not (log and hasattr(listener, "IS_TRUSTED")):
+        return listener
+
+    # Work around a bogus warning in pynput and Darwin
+    BOGUS_WARNING = (
+        "This process is not trusted! Input event monitoring will not be possible"
+        " until it is added to accessibility clients."
+    )
+    warning_ = log.warning
+
+    @wraps(warning_)
+    def warning(a: str, *args: Any, **kwargs: Any) -> None:
+        if not a.strip() or a.replace(BOGUS_WARNING, "").strip() or args or kwargs:
+            warning_(a, *args, **kwargs)
+
+    log.warning = warning
+    return listener
---
+++ tuney/keyboard/queue.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import dataclasses as dc
+import sys
+import threading
+import traceback
+from functools import cached_property, wraps
+from queue import Empty, Queue
+from typing import Any, Callable, TypeAlias
+
+from pynput import keyboard
+
+from . import Callback, Key, KeyAction
+from .listener import KeyboardListener
+
+
+@dc.dataclass
+class KeyboardQueue:
+    callback: Callback
+    timeout: float = 0.01
+    running: bool = False
+
+    def start(self) -> None:
+        self.running = True
+        try:
+            self._thread.start()
+            self._listener.start()
+        finally:
+            self.running = False
+
+    def join(self) -> None:
+        self._thread.join()
+
+    @cached_property
+    def _listener(self) -> KeyboardListener:
+        return KeyboardListener(self._queue.put)
+
+    def _target(self) -> None:
+        try:
+            while self.running:
+                try:
+                    key_action = self._queue.get(timeout=self.timeout)
+                except Empty:
+                    continue
+                if not key_action:
+                    break
+                self.callback(key_action)
+            self.callback(KeyAction())
+        except Exception:
+            print("THREAD TERMINATED", file=sys.stderr)
+            traceback.print_exc()
+
+    @cached_property
+    def _thread(self) -> threading.Thread:
+        return threading.Thread(target=self._target)
+
+    @cached_property
+    def _queue(self) -> Queue[KeyAction]:
+        return Queue()
