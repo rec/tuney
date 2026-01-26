91313362c7400f41b01d090481687aaf9e771a03
Tom Ritchford <tom@swirly.com>
2026-01-26 13:24:09 +0100

    Merge playback.py into player.py

--- tuney/audio/player.py
+++ tuney/audio/player.py
@@ -3,10 +3,46 @@ from __future__ import annotations
 import dataclasses as dc
 from abc import ABC, abstractmethod
 from functools import cached_property
+from threading import Event
+from typing import Protocol
+
+from sounddevice import CallbackStop, OutputStream
 
 from . import Data
 from .device_config import DeviceConfig
-from .playback import Playback
+from .runnable import Runnable
+
+
+class Filler(Protocol):
+    def __call__(self, out: Data, frame_size: int) -> bool: ...
+
+
+class Playback(Runnable):
+    def __init__(self, config: DeviceConfig, fill: Filler) -> None:
+        self.config = config
+        self.fill = fill
+        self._event = Event()
+
+    def callback(self, out: Data, frame_size: int, time: float, status: str) -> None:
+        if status:
+            print("Playback", status)  # TODO:
+
+        if not self.fill(out, frame_size) or not self.is_running:
+            self.stop()
+            raise CallbackStop
+
+    def _run(self):
+        with self.stream:
+            self._event.wait()
+
+    @cached_property
+    def stream(self) -> OutputStream:
+        kwargs = dc.asdict(self.config)
+        return OutputStream(
+            callback=self.callback,
+            finished_callback=self._event.set,
+            **kwargs,
+        )
 
 
 @dc.dataclass
