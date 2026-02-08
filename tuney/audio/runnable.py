ab4b6ae9db06fae603026768fe7fc38d52920546
Tom Ritchford <tom@swirly.com>
2026-02-08 16:38:15 +0100

    Teach Runnable how to create a thread

--- tuney/audio/runnable.py
+++ tuney/audio/runnable.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from threading import Thread
 
 
 class Runnable(ABC):
@@ -9,14 +10,29 @@ class Runnable(ABC):
         finally:
             self.stop()
 
+    @abstractmethod
+    def _run(self) -> None: ...
+
     @property
     def is_running(self) -> bool:
         return self._running
 
+    def start(self) -> Thread:
+        t = Thread(target=self.run)
+        t.start()
+        return t
+
     def stop(self) -> None:
         self._running = False
 
-    @abstractmethod
-    def _run(self) -> None: ...
-
     _running: bool = False
+
+
+class Loop(Runnable):
+    def run(self) -> None:
+        self._running = True
+        try:
+            while self._running:
+                self._run()
+        finally:
+            self.stop()
