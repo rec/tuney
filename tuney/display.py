62986abe0dd31e21cbe0e44085e56dfe9778d9da
Tom Ritchford <tom@swirly.com>
2026-07-03 15:48:06 +0200

    Make Display a BaseModel

--- tuney/display.py
+++ tuney/display.py
@@ -1,10 +1,16 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, GetCoreSchemaHandler
+from pydantic_core import CoreSchema
 
 
-@dataclass(frozen=True)
-class Display:
+class Display(BaseModel, frozen=True):
     hidden: bool = False
     general: bool = False
     beginner: bool = False
     row: int | None = None
     order: int = 0
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source: object, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        return handler(source)
