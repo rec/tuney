24b3564197534a0e47ea5aa531e5b05e4057b2f3
Tom Ritchford <tom@swirly.com>
2026-07-07 14:01:23 +0200

    Add scale.cents(), .uncents()

--- tuney/scale/__init__.py
+++ tuney/scale/__init__.py
@@ -1,1 +1,12 @@
+import math
+from fractions import Fraction
+
 type NoteNumber = int  # May be negative
+
+
+def cents(f: float | Fraction) -> float:
+    return math.exp2(float(f) / 1200)
+
+
+def uncents(c: float) -> float:
+    return math.log2(c) * 1200
