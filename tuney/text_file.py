d7564a835fca90c28c18cadc0da472034294b766
Tom Ritchford <tom@swirly.com>
2026-07-07 11:36:32 +0200

    Catch fewer exceptions in text_file.py

--- tuney/text_file.py
+++ tuney/text_file.py
@@ -5,17 +5,9 @@ from pathlib import Path
 import chardet
 
 
-class UnreadableTextFile(ValueError):
-    pass
-
-
 def read_text_file(path: Path) -> str:
-    try:
-        data = path.read_bytes()
-        encoding = chardet.detect(data).get('encoding')
-        if not isinstance(encoding, str):
-            raise UnicodeError('could not detect text encoding')
-        return data.decode(encoding)
-    except Exception as error:
-        message = f'Could not read text file {path}: unreadable file'
-        raise UnreadableTextFile(message) from error
+    data = path.read_bytes()
+    encoding = chardet.detect(data).get('encoding')
+    if not isinstance(encoding, str):
+        raise UnicodeError('could not detect text encoding')
+    return data.decode(encoding)
