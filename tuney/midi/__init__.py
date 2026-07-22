b609cd810984adc970cf03e4d260bf516e448b47
Tom Ritchford <tom@swirly.com>
2026-07-22 12:49:01 +0200

    Inline midi package exports

--- tuney/midi/__init__.py
+++ tuney/midi/__init__.py
@@ -1,3 +1,4 @@
+# ruff: noqa: F401
 from .file import MIDI_FILE_TICKS_PER_BEAT, is_midi_file, write_midi_file
 from .model import MIDI, Listener, MIDIIn, MidiOut
 from .ports import (
@@ -8,19 +9,3 @@ from .ports import (
     output_names,
     output_names_json,
 )
-
-__all__ = [
-    'INTERNAL_LIST_MIDI_INPUTS',
-    'INTERNAL_LIST_MIDI_OUTPUTS',
-    'MIDI',
-    'MIDIIn',
-    'MIDI_FILE_TICKS_PER_BEAT',
-    'Listener',
-    'MidiOut',
-    'input_names',
-    'input_names_json',
-    'is_midi_file',
-    'output_names',
-    'output_names_json',
-    'write_midi_file',
-]
